75c7d6c6d5c924a2db79794e8e318854df8a6672
rv-work <rv.dthought@gmail.com>
2026-01-13 17:52:27 +0530

    DB Erroe

--- backend/index.js
+++ backend/index.js
@@ -4,58 +4,43 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 import cookieParser from "cookie-parser"
 import path from "path"
+import { fileURLToPath } from "url"
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import taskRoutes from "./routes/task.route.js"
 import reportRoutes from "./routes/report.route.js"
-import { fileURLToPath } from "url"
 
 dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Database is connected")
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
 const app = express()
 
-// Middleware to handle cors
+// Middleware
 app.use(
   cors({
-    origin: process.env.FRONT_END_URL || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: process.env.FRONT_END_URL,
     credentials: true,
   })
 )
 
-// Middleware to handle JSON object in req body
 app.use(express.json())
-
 app.use(cookieParser())
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!")
-})
-
+// Routes
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/tasks", taskRoutes)
 app.use("/api/reports", reportRoutes)
 
-// serve static files from "uploads" folder
+// Static files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
+// Error middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
-
   const message = err.message || "Internal Server Error"
 
   res.status(statusCode).json({
@@ -64,3 +49,19 @@ app.use((err, req, res, next) => {
     message,
   })
 })
+
+
+const PORT = process.env.PORT || 3000
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Database connected")
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error("❌ DB connection failed", err)
+  })
